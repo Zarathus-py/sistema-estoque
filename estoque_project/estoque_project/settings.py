611a1ecfe90a378631d593e0b2bdc26d3af7b8b7
Zarathus | Dev Python <rodzmaciel21@gmail.com>
2025-04-04 21:43:22 -0300

    Update settings.py

--- estoque_project/estoque_project/settings.py
+++ estoque_project/estoque_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,8 +79,7 @@ WSGI_APPLICATION = 'estoque_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+	'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
 }
 
