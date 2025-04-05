2c8ac361985939e2fce6c2ca5b986255d225d114
Zarathus <rodzmaciel21@gmail.com>
2025-04-04 22:58:01 -0300

    Reorganiza estrutura de pastas e renomeia app

--- sistema-estoque/estoque/settings.py
+++ sistema-estoque/estoque/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'estoque',
+    'app_estoque',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +79,7 @@ WSGI_APPLICATION = 'estoque_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+	'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
 }
 
@@ -117,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
